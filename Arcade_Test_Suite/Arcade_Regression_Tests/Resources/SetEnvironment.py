032bf1b4f69f0597f5db4e56a3d3912c66ac9896
Sudha Menon <sudha.menon@rallyhealth.com>
2016-04-28 14:25:48 -0500

    moving all tests into regression folder

--- Arcade_Test_Suite/Arcade_Regression_Tests/Resources/SetEnvironment.py
+++ Arcade_Test_Suite/Arcade_Regression_Tests/Resources/SetEnvironment.py
@@ -4,7 +4,7 @@ import inspect
 import re
 
 COMMON = {
-    'FALSE': {'RALLY_SELENIUM_BROWSER': 'FireFox',
+    'FALSE': {'RALLY_SELENIUM_BROWSER': 'Firefox',
               'RALLY_REMOTE_URL': '',
               'RALLY_DESIRED_CAPABILITIES': 'none',
               'RALLY_SELENIUM_TIMEOUT': '10',
@@ -22,13 +22,6 @@ COMMON = {
         'RALLY_REMOTE_URL': 'http://rallytest:d2ae1b7f-4fcb-4f9e-b0d7-d2b735d25262@ondemand.saucelabs.com:80/wd/hub',
         'RALLY_DESIRED_CAPABILITIES': 'name:Rally Arcade CHROME Automated Test Suite,platform:Windows 7,browserName:Chrome,version:latest-1 ,javascriptEnabled:True',
         'RALLY_SELENIUM_TIMEOUT': '10',
-        'RALLY_SELENIUM_SPEED': '.4'
-            },
-        'IE': {
-        'RALLY_SELENIUM_BROWSER': 'IE',
-        'RALLY_REMOTE_URL': 'http://rallytest:d2ae1b7f-4fcb-4f9e-b0d7-d2b735d25262@ondemand.saucelabs.com:80/wd/hub',
-        'RALLY_DESIRED_CAPABILITIES': 'name:Rally Arcade IE Automated Test Suite,platform:Windows 10,browserName:internet explorer,version:latest ,javascriptEnabled:True',
-        'RALLY_SELENIUM_TIMEOUT': '1',
         'RALLY_SELENIUM_SPEED': '.4'
             },
         'ALL' :{
@@ -55,6 +48,7 @@ def get_variables(environment, on_demand, browser):
     ondem = str(on_demand).upper()
     browser= str(browser).upper()
     print ondem
+    print browser
 
     if (passedENV.upper() == 'LOCAL'):
         url = 'localhost:10088'
@@ -66,21 +60,15 @@ def get_variables(environment, on_demand, browser):
     else:
         url = 'https://' + passedENV.lower() + '-arcade.werally.in'
 
+    print   url
+
 
     ENVS = {
 
         'RALLY_TEST_URL': url
     }
-       # if (browser == 'ALL'):
-       #      for key,val in COMMON[ondem][browser].items():
-       #     print  " ******KEY*****"
-       #     print("{} = {}".format(key, val))
 
-
-
-
-
-
+    print   url
     # Checks to see if the Environment and OnDemand variables are used
     if re.match('\\$\\{.*\\}', ondem) != None:
         env = COMMON.keys()[0]
@@ -90,13 +78,15 @@ def get_variables(environment, on_demand, browser):
             'Undefined on demand: ' + str(on_demand) + '. Try one of these: ' + ', '.join(
                 sorted(COMMON.keys())))
 
+
+
     logger.info(msg_prefix + 'Defining arcade web variables for environment ' + passedENV)
     logger.console(msg_prefix + 'Defining arcade web variables for environment ' + passedENV)
+    print       "dict here" + dict(list(COMMON[ondem][browser].items()))
+    print       list(ENVS.items())
 
     print       '*************************************************'
-    print       dict(list(COMMON[ondem][browser].items()) + list(ENVS.items()))
+    print       dict(list(COMMON[ondem][browser].items()) +list(ENVS.items()))
     print       '*************************************************'
-    return dict(list(COMMON[ondem][browser].items()) + list(ENVS.items()))
+    return  dict(list(COMMON[ondem][browser].items()) +list(ENVS.items()))
 
-def get_test():
-    print "am here"
