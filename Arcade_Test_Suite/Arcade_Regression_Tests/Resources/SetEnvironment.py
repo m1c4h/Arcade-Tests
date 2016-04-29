96e46d72057ea7b0ec17dd15a62e320d2a2bb2e2
Sudha Menon <sudha.menon@rallyhealth.com>
2016-04-29 17:18:35 -0500

    added javascript to focus to overcome chromedriver bug

--- Arcade_Test_Suite/Arcade_Regression_Tests/Resources/SetEnvironment.py
+++ Arcade_Test_Suite/Arcade_Regression_Tests/Resources/SetEnvironment.py
@@ -21,7 +21,14 @@ COMMON = {
         'CHROME': {
         'RALLY_SELENIUM_BROWSER': 'CHROME',
         'RALLY_REMOTE_URL': 'http://rallytest:d2ae1b7f-4fcb-4f9e-b0d7-d2b735d25262@ondemand.saucelabs.com:80/wd/hub',
-        'RALLY_DESIRED_CAPABILITIES': 'name:Rally Arcade CHROME Automated Test Suite,platform:Windows 7,browserName:Chrome,version:latest-1 ,javascriptEnabled:True',
+        'RALLY_DESIRED_CAPABILITIES': 'name:Rally Arcade CHROME Automated Test Suite,platform:Windows 10,browserName:Chrome,version:latest-1,javascriptEnabled:True',
+        'RALLY_SELENIUM_TIMEOUT': '10',
+        'RALLY_SELENIUM_SPEED': '.4'
+            },
+        'IE': {
+        'RALLY_SELENIUM_BROWSER': 'IE',
+        'RALLY_REMOTE_URL': 'http://rallytest:d2ae1b7f-4fcb-4f9e-b0d7-d2b735d25262@ondemand.saucelabs.com:80/wd/hub',
+        'RALLY_DESIRED_CAPABILITIES': 'name:Rally Arcade IE Automated Test Suite,platform:Windows 10,browserName:IE,version:latest-1 ,javascriptEnabled:True',
         'RALLY_SELENIUM_TIMEOUT': '10',
         'RALLY_SELENIUM_SPEED': '.4'
             },
